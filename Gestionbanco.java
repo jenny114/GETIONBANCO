75b0ae35c27e0ada18e582839d4697460f8fa314
jenny114 <32575395+jenny114@users.noreply.github.com>
2017-10-15 20:32:31 -0500

    Add files via upload

---
+++ Gestionbanco.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista.TBBANCO;
+
+import vista.login;
+
+/**
+ *
+ * @author SENA
+ */
+public class Gestionbanco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        login op=new login();
+        op.setVisible(true);
+    }
+    
+}
